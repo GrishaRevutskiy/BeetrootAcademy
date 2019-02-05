d5a5cce597f2906c97ae787dbc78936dcb3ac8e2
Grisha Revutskiy <revytskiugg@gmail.com>
2019-02-05 20:02:22 +0200

    new

---
+++ Adaptive/js/script.js
@@ -0,0 +1,13 @@
+;(function($){
+	"use strict";
+
+	$(function(){
+		$('.slider').slick({
+	       	  slidesToShow: 4,
+			  slidesToScroll: 1,
+			  autoplay: true,
+			  autoplaySpeed: 2000,
+			});
+	
+	})
+})(jQuery);
